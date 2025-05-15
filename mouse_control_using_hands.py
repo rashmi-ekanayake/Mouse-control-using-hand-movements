12ef34f1fc117af50d8841e7fec6fb5b46e0ef71
RashmiAyodhya <jennie20.23rak@gmail.com>
2025-05-15 19:14:56 +0530

    Add real-time hand tracking using MediaPipe.py

--- mouse_control_using_hands.py
+++ mouse_control_using_hands.py
@@ -2,9 +2,19 @@ import cv2
 import mediapipe 
 import pyautogui
 
+capture_hands = mediapipe.solutions.hands.Hands()
+drawing_option = mediapipe.solutions.drawing_utils
 camera = cv2.VideoCapture(0)
 while True:
     _,image = camera.read()
+     image = cv2.flip(image, 1)
+    rgb_image = cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
+    output_hands = capture_hands.process(rgb_image)
+    all_hands = output_hands.multi_hand_landmarks
+
+    if all_hands:
+        for hand in all_hands:
+            drawing_option.draw_landmarks(image, hand)
     cv2.imshow("Hand movement video capture", image)
     key = cv2.waitKey(100)
     if key == 27:
