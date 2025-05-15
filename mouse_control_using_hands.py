92460e3810415e8d22b403687b35eb77f83e20f2
RashmiAyodhya <jennie20.23rak@gmail.com>
2025-05-15 19:25:05 +0530

    Convert hand landmark positions to screen coordinates.py

--- mouse_control_using_hands.py
+++ mouse_control_using_hands.py
@@ -7,14 +7,21 @@ drawing_option = mediapipe.solutions.drawing_utils
 camera = cv2.VideoCapture(0)
 while True:
     _,image = camera.read()
-     image = cv2.flip(image, 1)
+    image_height,image_width,_ = image.shape
+
+    image = cv2.flip(image, 1)
     rgb_image = cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
     output_hands = capture_hands.process(rgb_image)
-    all_hands = output_hands.multi_hand_landmarks
 
+    all_hands = output_hands.multi_hand_landmarks
     if all_hands:
         for hand in all_hands:
             drawing_option.draw_landmarks(image, hand)
+            one_hand_landmarks = hand.landmark
+            for id, lm in enumerate(one_hand_landmarks):
+                x = int(lm.x * image_width)
+                y = int(lm.y * image_height)
+                #print(x,y)
     cv2.imshow("Hand movement video capture", image)
     key = cv2.waitKey(100)
     if key == 27:
